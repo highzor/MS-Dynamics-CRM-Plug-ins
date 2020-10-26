d4f887ca25c707f02c9a789b0eea62da534d46e2
highzor <52636901+highzor@users.noreply.github.com>
2020-10-26 14:32:35 +0300

    Update WorkFlowAction1.cs

--- WorkFlowAction1.cs
+++ WorkFlowAction1.cs
@@ -4,6 +4,8 @@ using Microsoft.Xrm.Sdk;
 using Microsoft.Xrm.Sdk.Workflow;
 using Microsoft.Crm.Sdk.Samples.EarlyBoundTypes;
 using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Crm.Sdk.Messages;
+using System;
 
 namespace Custom.WorkFlowAction1
 {
@@ -19,15 +21,27 @@ namespace Custom.WorkFlowAction1
             IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
             IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
             IOrganizationService service = serviceFactory.CreateOrganizationService(workflowContext.InitiatingUserId);
-            Email myMail = GetEmail(service, context);
-            var partyInf = myMail.To.First();
-            myMail.Description = $"<p><font size='5' color='red' face='Arial'>Ваша компания зарегистрирована {partyInf.PartyId.Name}. Будем рады продолжить сотрудничество.</font></p>";
-            service.Update(myMail);
+            Email email = GetEmail(service, context);
+            var partyInf = email.To.First();
+            email.Description = $"<p><font size='5' color='red' face='Arial'>Ваша компания зарегистрирована {partyInf.PartyId.Name}. Будем рады продолжить сотрудничество.</font></p>";
+            service.Update(email);
+            SendEmail(service, email);
         }
         private Email GetEmail(IOrganizationService service, CodeActivityContext context)
         {
             Email myMail = (Email)service.Retrieve(Email.EntityLogicalName, DescEmail.Get(context).Id, new ColumnSet("description", "to"));
             return myMail;
+        }
+        private void SendEmail(IOrganizationService service, Email email)
+        {
+            Guid emailId = email.Id;
+            SendEmailRequest sendEmailreq = new SendEmailRequest
+            {
+                EmailId = emailId,
+                TrackingToken = "",
+                IssueSend = true
+            };
+            SendEmailResponse sendEmailresp = (SendEmailResponse)service.Execute(sendEmailreq);
         }
     }
 }
